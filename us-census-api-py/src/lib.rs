0b427df0cdb2579e0842b14455f45b9e0684a90a
Robert Fitzgerald <robfitzgerald@gmail.com>
2024-09-11 12:52:03 -0600

    python api as df result

--- us-census-api-py/src/lib.rs
+++ us-census-api-py/src/lib.rs
@@ -1,9 +1,8 @@
-// #![doc = include_str!("doc.md")]
-
-mod us_census_python_app;
 use pyo3::prelude::*;
+mod us_census_python_app;
 
 #[pymodule]
+#[pyo3(name = "uscensus")]
 fn us_census_api_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(us_census_python_app::wac_tiger, m)?)?;
     Ok(())
