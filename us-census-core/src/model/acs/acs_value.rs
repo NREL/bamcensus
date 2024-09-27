20305aaa8b4bbc3344a50156917584d3f8449c9e
rfitzger <robert.fitzgerald@nrel.gov>
2024-09-27 13:46:11 -0600

    safer numeric decoding

--- us-census-core/src/model/acs/acs_value.rs
+++ us-census-core/src/model/acs/acs_value.rs
@@ -12,6 +12,28 @@ impl AcsValue {
     pub fn new(name: String, value: serde_json::Value) -> AcsValue {
         AcsValue { name, value }
     }
+
+    /// to numeric operation.
+    ///
+    /// # Background
+    ///
+    /// `serde_json::Value.as_f64()` is for values that are actual JSON number types, but when
+    /// dealing with ACS values, they are always string types, and so the built-in `as_f64()`
+    /// method gives the following error, for example:
+    ///
+    ///   > cannot parse acs value for B01001_001E as number, found \"3889\""
+    ///
+    /// this method also ensures we are dealing with a trimmed string before attempting to parse it as a number.
+    pub fn as_f64_safe(&self) -> Result<f64, String> {
+        let value_str = self
+            .value
+            .as_str()
+            .ok_or_else(|| format!("failed to decode value as string: {}", self.value))?;
+        value_str
+            .trim()
+            .parse::<f64>()
+            .map_err(|e| format!("failed to decode value as f64: {}", e))
+    }
 }
 
 impl Display for AcsValue {
